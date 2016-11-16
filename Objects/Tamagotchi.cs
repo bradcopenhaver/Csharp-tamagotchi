a587ec346c33129fd5ffcf241b45979d60c3a6d2
Brad Copenhaver and Ryan Mathisen <brad+ryan@>
2016-11-16 15:14:54 -0800

    add check for food < 0 and isAlive bool to Pet object, along with a death check and a 'RIP' concatenation if animal passed away

--- Objects/Tamagotchi.cs
+++ Objects/Tamagotchi.cs
@@ -11,6 +11,7 @@ namespace Tamagotchi.Objects
     private int _currentRest = 100;
     private int _Id;
     private string _petType;
+    private bool _isAlive = true;
     private static List<Pet> _petList = new List<Pet>{};
 
     public Pet (string PetName, string PetType)
@@ -70,16 +71,34 @@ namespace Tamagotchi.Objects
     {
       return _petList[searchId-1];
     }
+
+    public static void CheckDeath() {
+      foreach (Pet pet in _petList) {
+        if (pet._isAlive == true && pet.GetFood() <= 0) {
+          System.Console.WriteLine("Dude, " + pet.GetName() + " died. Wow.");
+          pet.SetName(pet.GetName() + "(R.I.P.)");
+          pet._isAlive = false;
+        }
+      }
+    }
+
     public static void PassTime()
     {
+      Random rnd1 = new Random();
       foreach(Pet pet in _petList)
       {
-        Random rnd = new Random();
-        pet.SetFood(pet.GetFood() - rnd.Next(5,10));
-        pet.SetLove(pet.GetLove() - rnd.Next(2, 12));
-        pet.SetRest(pet.GetRest() - rnd.Next(1, 5));
+        if (pet._isAlive == true) {
+          Random rnd = new Random(pet.GetId()+rnd1.Next(1,100));
+          pet.SetFood(pet.GetFood() - rnd.Next(5,10));
+          pet.SetLove(pet.GetLove() - rnd.Next(2, 12));
+          pet.SetRest(pet.GetRest() - rnd.Next(1, 5));
+        }
       };
+      CheckDeath();
     }
+
+
+
     public int GetId() {
       return _Id;
     }
